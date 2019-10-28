078cebfbc3cf9e732b330e3b3cb31863dd3451ae
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-10-28 17:05:16 +0100

    get all users

--- index.js
+++ index.js
@@ -9,12 +9,24 @@ const server = express()
 server.use(cors())
 server.use(express.json())
 
+server.get('/api/users', getAllUsers)
 server.get('*', handleDefaultrequest)
+
+function getAllUsers(req, res) {
+    db.find()
+    .then(data => {
+        console.log(data);
+        res.status(200).json(data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
 
 function handleDefaultrequest(req, res) {
     res.json('hello world')
 }
 
 server.listen(process.env.port || 3000, () => {
-    console.log('Listening to' + (process.env.port || 3000))
+    console.log('Listening to ' + (process.env.port || 3000))
 })
