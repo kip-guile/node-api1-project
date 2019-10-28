39af7f94a29fb2fdefe4c880585aa21bac48aae0
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-10-28 16:56:20 +0100

    index.js setup

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+const express = require('express')
+const cors = require('cors')
+
+const db = require('./data/db')
+
+const server = express()
+
+server.use(cors())
+server.use(express.json())
+
+server.get('*', handleDefaultrequest)
+
+function handleDefaultrequest(req, res) {
+    res.json('hello world')
+}
+
+server.listen(process.env.port || 3000, () => {
+    console.log('Listening to' + (process.env.port || 3000))
+})
